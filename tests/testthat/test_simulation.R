8eefd9f9288d3c3a54ee614443e959257ff39bea
Benjamin Allévius <benjak@math.su.se>
2018-05-22 11:31:18 +0200

    Tests complete.

--- tests/testthat/test_simulation.R
+++ tests/testthat/test_simulation.R
@@ -39,12 +39,14 @@ test_that("ar1_sim_irregular", {
 
 
 test_that("ar1_sim_conditional", {
-  s <- 3
+  s <- 0.5
   r <- 0.5
   t_pred <- c(1, 3, 6:7, 19)
   t_obs <- c(2, 5, 11:14)
+  # t_pred <- 1:3
+  # t_obs <- 4:7
   mu_obs <- t_obs
-  x_obs <- mu_obs + rnorm(length(t_obs))
+  x_obs <- mu_obs + rnorm(length(t_obs), sd = s)
   mu_pred <- t_pred
 
   S_o <- ar1_cov_xy(t_obs, t_obs, r, s)
@@ -52,11 +54,21 @@ test_that("ar1_sim_conditional", {
   A <- t(solve(S_o, S_op))
   mu_po <- mu_pred + A %*% (x_obs - mu_obs)
   S_po <- Matrix::symmpart(ar1_cov_xy(t_pred, t_pred, r, s) - A %*% S_op)
+  # S_po <- ar1_cov_xy(t_pred, t_pred, r, s) - A %*% S_op
 
-  d <- rep(0, length(t_pred))
-  for (i in 1:100000) {
-    p1 <- ar1_sim_conditional(t_pred, t_obs, x_obs, r, s, mu_pred, mu_obs)
-    p2 <- MASS::mvrnorm(1, mu_po, S_po)
-    d <- d + (p1 - p2 - d) / i
+  n <- 1000
+  m1 <- rep(0, length(t_pred))
+  m2 <- rep(0, length(t_pred))
+  # dm <- numeric(n)
+  for (i in 1:n) {
+    x1 <- ar1_sim_conditional(t_pred, t_obs, x_obs, r, s, mu_pred, mu_obs)
+    x2 <- as.vector(mvtnorm::rmvnorm(1, mu_po, S_po))
+    m1 <- m1 + (x1 - m1) / i
+    m2 <- m2 + (x2 - m2) / i
   }
+  d <- max(abs(2*atan(m1 / m2) - pi/2))
+  # plot(dm, type='l'); abline(h = 0)
+  # summary(dm)
+  eps <- 10^(-log10(n)+2)
+  expect_equal(d, 0, tolerance = eps)
 })
